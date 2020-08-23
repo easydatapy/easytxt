979b6855d68f8f2ea15451c6444bb7602de5fe01
sitegroove <grabnarrok@gmail.com>
2020-08-23 17:57:04 +0200

    initial commit

---
+++ tests/factory/raw_sentences_samples.py
@@ -0,0 +1,14 @@
+english = [
+    (
+        'Mr. John and Ms. Sarah are here. Say hello!!!',
+        ['Mr. John and Ms. Sarah are here.', 'Say hello!!!']
+    ),
+    (
+        '* camera * notebook * photo',
+        ['camera', 'notebook', 'photo']
+    ),
+    (
+        '- camera - notebook - photo',
+        ['camera', 'notebook', 'photo']
+    )
+]
