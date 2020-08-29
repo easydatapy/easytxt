c69009335cbe441aa095b39a99b4f88b61834a83
sitegroove <grabnarrok@gmail.com>
2020-08-29 20:03:56 +0200

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easytxt',
-    version='0.0.1',
+    version='0.0.2',
     description='Text manipulation and normalization library.',
     long_description=open('README.rst').read(),
     long_description_content_type="text/x-rst",
