24096654be3cce9b8ea0b4c803429c33e48a5027
sitegroove <grabnarrok@gmail.com>
2020-09-08 23:36:20 +0200

    improve project settings

--- setup.py
+++ setup.py
@@ -1,29 +1,18 @@
+import re
 from setuptools import setup, find_packages
+
+
+(__version__, ) = re.findall(r"__version__.*\s*=\s*[\"]([^']+)[\"]",
+                             open('easytxt/__init__.py').read())
+
 
 setup(
     name='easytxt',
-    version='0.0.4',
-    description='Text manipulation and normalization library.',
-    long_description=open('README.rst').read(),
-    long_description_content_type="text/x-rst",
-    author='Rok Grabnar',
-    author_email='grabnarrok@gmail.com',
-    url='https://github.com/sitegroove/easytxt',
+    version=__version__,
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'ftfy',
         'pyquery',
         'number-parser'
-    ],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
+    ]
 )
